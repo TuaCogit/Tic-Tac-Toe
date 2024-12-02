42645a8943cb36fc11b1f9c2938e9f1aa9934bbf
TuaCogit <84384646+TuaCogit@users.noreply.github.com>
2024-12-03 02:53:31 +0300

    Add files via upload

---
+++ TiTaTo/TiTaTo/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿
+namespace TiTaTo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Honeydew;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.button1.Location = new System.Drawing.Point(19, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 0;
+            this.button1.Tag = "0";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button_click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Honeydew;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.Color.LimeGreen;
+            this.button2.Location = new System.Drawing.Point(125, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 100);
+            this.button2.TabIndex = 1;
+            this.button2.Tag = "1";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button_click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Honeydew;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.ForeColor = System.Drawing.Color.LimeGreen;
+            this.button3.Location = new System.Drawing.Point(231, 27);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 100);
+            this.button3.TabIndex = 2;
+            this.button3.Tag = "2";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button_click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Honeydew;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.ForeColor = System.Drawing.Color.LimeGreen;
+            this.button4.Location = new System.Drawing.Point(19, 133);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 100);
+            this.button4.TabIndex = 3;
+            this.button4.Tag = "3";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button_click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Honeydew;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button5.ForeColor = System.Drawing.Color.LimeGreen;
+            this.button5.Location = new System.Drawing.Point(125, 133);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 100);
+            this.button5.TabIndex = 4;
+            this.button5.Tag = "4";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button_click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Honeydew;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button6.ForeColor = System.Drawing.Color.LimeGreen;
+            this.button6.Location = new System.Drawing.Point(231, 133);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 100);
+            this.button6.TabIndex = 5;
+            this.button6.Tag = "5";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button_click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Honeydew;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button7.ForeColor = System.Drawing.Color.LimeGreen;
+            this.button7.Location = new System.Drawing.Point(19, 239);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 100);
+            this.button7.TabIndex = 6;
+            this.button7.Tag = "6";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button_click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Honeydew;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button8.ForeColor = System.Drawing.Color.LimeGreen;
+            this.button8.Location = new System.Drawing.Point(125, 239);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 100);
+            this.button8.TabIndex = 7;
+            this.button8.Tag = "7";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button_click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Honeydew;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button9.ForeColor = System.Drawing.Color.LimeGreen;
+            this.button9.Location = new System.Drawing.Point(231, 239);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 100);
+            this.button9.TabIndex = 8;
+            this.button9.Tag = "8";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button_click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(359, 401);
+            this.panel1.TabIndex = 9;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button9);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.button8);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Font = new System.Drawing.Font("Bookman Old Style", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.ForeColor = System.Drawing.Color.DarkGreen;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(359, 350);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ходите Первым";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Gainsboro;
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox1.Font = new System.Drawing.Font("Bookman Old Style", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.groupBox1.Location = new System.Drawing.Point(0, 350);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(359, 51);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Чем играть";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(239, 21);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(45, 25);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "O";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(41, 21);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(43, 25);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "X";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGray;
+            this.ClientSize = new System.Drawing.Size(359, 401);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Крестики - Нолики";
+            this.panel1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ TiTaTo/TiTaTo/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TiTaTo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            buttonsArr = new List<Button> { button1, button2, button3, button4, button5, button6, button7, button8, button9 };
+            radioButton1.Checked = true;
+        }
+        List<Button> buttonsArr; //массив ячеек поля
+        //обработчик нажатия на клетку поля
+        private void button_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            move(Convert.ToInt32(button.Tag), human);
+        }
+        //начальное состояние поля
+        string[] board = { "0", "1", "2", "3", "4", "5", "6", "7", "8" };
+        string human = "X"; //пользователь по умолчанию
+        string comp = "O"; //компьютер по умолчанию
+        int iter = 0;
+        int roundNum = 0; //номер раунда        
+        class steps //объект хода  
+        {
+            public string index;
+            public int points;
+        }
+        //обработка хода
+        public void move(int id, string player)
+        {
+            if (board[id] != "X" && board[id] != "O")
+            {
+                roundNum++;
+                board[id] = player;
+                buttonsArr[id].Enabled = false;
+                buttonsArr[id].Text = human;
+
+                if (winning(board, player))
+                {
+                    DialogResult result = MessageBox.Show("Победили ВЫ!", "Вот так конец!");
+                    if (result == DialogResult.OK) newGame();
+                    return;
+                }
+                else if (roundNum > 8)
+                {
+                    DialogResult result = MessageBox.Show("Да это же НИЧЬЯ!", "Вот так конец!");
+                    if (result == DialogResult.OK) newGame();
+                    return;
+                }
+                else
+                {
+                    roundNum++;
+                    var index = Convert.ToInt32(minimax(board, comp).index);
+                    buttonsArr[index].Enabled = false;
+                    buttonsArr[index].Text = comp;
+                    board[index] = comp;
+
+                    if (winning(board, comp))
+                    {
+                        DialogResult result = MessageBox.Show("Компьютер ПОБЕДИЛ!", "Вот так конец!");
+                        if (result == DialogResult.OK) newGame();
+                        return;
+                    }
+                    else if (roundNum == 0)
+                    {
+                        DialogResult result = MessageBox.Show("Да это же НИЧЬЯ!", "Вот так конец!");
+                        if (result == DialogResult.OK) newGame();
+                        return;
+                    }
+                }
+            }
+        }
+        void newGame() //очистка поля для новой игры
+        {
+            roundNum = 0; int i = 0;
+            string[] clearBoard = { "0", "1", "2", "3", "4", "5", "6", "7", "8" };
+            foreach (string r in clearBoard)
+            {
+                board[i] = r;
+                i++;
+            }
+            foreach (var bb in buttonsArr)
+            {
+                bb.Enabled = true;
+                bb.Text = "";
+            }
+        }
+        //алгоритм минимакса
+        steps minimax(string[] reboard, string player)
+        {
+            iter++;
+            steps newStep = new steps();
+            List<int> emptyСellsArr = (emptyСells(reboard)).ToList();
+            if (winning(reboard, human))
+            {
+                newStep.points = -1;
+                return newStep;
+            }
+            else if (winning(reboard, comp))
+            {
+                newStep.points = 1;
+                return newStep;
+            }
+            else if (emptyСellsArr.Count == 0)
+            {
+                newStep.points = 0;
+                return newStep;
+            }
+            List<steps> stepsArr = new List<steps>();
+            for (var i = 0; i < emptyСellsArr.Count; i++)
+            {
+                steps step = new steps();
+                step.index = reboard[emptyСellsArr[i]];
+                reboard[emptyСellsArr[i]] = player;
+
+                if (player == comp)
+                {
+                    var minimaxValue = minimax(reboard, human);
+                    step.points = minimaxValue.points;
+                }
+                else
+                {
+                    var minimaxValue = minimax(reboard, comp);
+                    step.points = minimaxValue.points;
+                }
+                reboard[emptyСellsArr[i]] = step.index;
+                stepsArr.Add(step);
+            }
+            int bestStep = 0;
+            if (player == comp)
+            {
+                var bestPoint = -100;
+                for (var i = 0; i < stepsArr.Count; i++)
+                {
+                    if (stepsArr[i].points > bestPoint)
+                    {
+                        bestPoint = stepsArr[i].points;
+                        bestStep = i;
+                    }
+                }
+            }
+            else
+            {
+                var bestPoint = 100;
+                for (var i = 0; i < stepsArr.Count; i++)
+                {
+                    if (stepsArr[i].points < bestPoint)
+                    {
+                        bestPoint = stepsArr[i].points;
+                        bestStep = i;
+                    }
+                }
+            }
+            return stepsArr[bestStep];
+        }
+        //метод нахождения пустых клеток
+        List<int> emptyСells(string[] reboard)
+        {
+            List<int> emptyСellsArr = new List<int>();
+            for (int i = 0; i < reboard.Length; i++)
+            {
+                if (reboard[i] != "X" && reboard[i] != "O") emptyСellsArr.Add(i);
+            }
+            return emptyСellsArr;
+        }
+        //проверка на соответствие выигрышным комбинациям
+        bool winning(string[] board, string player)
+        {
+            if (
+              (board[0] == player && board[1] == player && board[2] == player) ||
+              (board[3] == player && board[4] == player && board[5] == player) ||
+              (board[6] == player && board[7] == player && board[8] == player) ||
+              (board[0] == player && board[3] == player && board[6] == player) ||
+              (board[1] == player && board[4] == player && board[7] == player) ||
+              (board[2] == player && board[5] == player && board[8] == player) ||
+              (board[0] == player && board[4] == player && board[8] == player) ||
+              (board[2] == player && board[4] == player && board[6] == player)
+            ) return true;
+            else return false;
+        }
+        //обработчик выбора Х или О пользователем
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton radio = (RadioButton)sender;
+            human = radio.Text;
+            if (radio.Text.Contains("X")) comp = "O";
+            else comp = "X";
+        }
+
+    }
+}
